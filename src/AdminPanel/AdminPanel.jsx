77d286f62aeeb6fdb0d3dbfa68033453eaa263c8
AdeHikmayati <pabdillah27@gmail.com>
2020-11-18 20:22:11 +0700

    Register

---
+++ src/AdminPanel/AdminPanel.jsx
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-router-dom";
+import './Registrasi.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+class AdminPanel extends Component {
+    render() {
+        return (
+           <div> </div>
+        )
+    }
+}
+export default AdminPanel;
--- src/Registrasi/Registrasi.jsx
+++ src/Registrasi/Registrasi.jsx
@@ -1,6 +1,8 @@
-import React, { Component , Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link, BrowserRouter } from "react-router-dom";
 import './Registrasi.css';
-
+import { Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -8,6 +10,7 @@ import './Registrasi.css';
 class Registrasi extends Component {
     render() {
         return (
+            <Router>
             <Fragment>
 
                 <div className="form-add-post">
@@ -21,17 +24,27 @@ class Registrasi extends Component {
                         <input type="text" name="title" placeholder="Email" ></input>
                         <input type="password" name="title" placeholder="Password" ></input>
                         <input type="Password" name="title" placeholder="Konfirmasi Password Kamu" ></input>
-                       
+                                <Dropdown>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    Pilih Status Anggota
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href="#/action-1">Pengajar</Dropdown.Item>
+                                    <Dropdown.Item href="#/action-2">Pelajar</Dropdown.Item>
+                                    
+                                </Dropdown.Menu>
+                                </Dropdown>
 
                     </div>
                                                     
-                    <button className="btn-submit" >Simpan</button>
-                    <p className='kelogin'>Kamu sudah mendaftar? Yuk langsung masuk!</p>
+                    <button className="btn-submit" >Daftar</button>
+                    <p className='kelogin'>Kamu sudah mendaftar? Yuk langsung  <Link to ="/login">masuk!</Link></p>
                 </div>
-               
+            
 
             </Fragment>
-              
+            </Router>
             
         )
     }
