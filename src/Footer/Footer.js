850a1b80a02707a8aea49d06726074c71402fc25
Devry04 <71996592+Devry04@users.noreply.github.com>
2020-11-22 22:19:38 +0700

    Footer

---
+++ src/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p>&copy; Copyright 2020 Kelompok 02 UGM</p>
+  </div>
+);
+
+export default Footer;
